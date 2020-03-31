99005025c869836c4f8604d2794acd99292f0ecf
Leslie <lesliec38@hotmail.fr>
2020-03-31 15:44:53 +0200

    change

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,17 +2,22 @@ import React from 'react';
 import App from '../App';
 import './style.css';
 
-const avatar = 'https://zupimages.net/up/20/14/w39m.png'
 
 function Contact() {
-    const Online = true;
+
+const avatar = 'https://zupimages.net/up/20/14/w39m.png'
+const Online = true;
+const connect = Online ? 'status-online' : 'status-offline';
+
     return (
         <div className="Contact">
-            <img src={avatar} className="avatar">
-            </img>
+            <img src={avatar} className="avatar"></img>
             <div>
                 <h4 className="name">Leslie Rouzier</h4>
-                    <span className="status status-text"><p className="status-online"></p>{Online ? 'Online' : 'Offline'}</span>
+                <div className="status">
+                    <span className={connect}></span>
+                    <p className="status-text">{Online == true? 'Online' : 'Offline'}</p>
+                </div>
             </div>
         </div>
     )
